ebb5a51e95b585aba584ff916089888d04384364
Arnapurna Sahu <arnapurnasahu59@gmail.com>
2026-02-20 15:17:59 +0530

    changes in vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // allows external connections
+    allowedHosts: ['margot-ethnological-axiologically.ngrok-free.dev'], // allow your ngrok URL
+  }
 })
